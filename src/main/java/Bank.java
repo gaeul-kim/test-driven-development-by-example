3d34caddf7035bbfc2742b3b16a8c458db1f1ca6
sangsik.kim <toheart@gmail.com>
2018-11-14 17:12:12 +0900

    코드 정리

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -4,9 +4,6 @@
 public class Bank {
 
     public Money reduce(Expression source, String to) {
-        if (source instanceof Money)
-            return (Money) source.reduce(to);
-        Sum sum = (Sum) source;
-        return sum.reduce(to);
+        return source.reduce(to);
     }
 }
