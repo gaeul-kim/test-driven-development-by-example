9947e68cd16887c6373d3b6fb0d8a3d4d6ae3585
sangsik.kim <toheart@gmail.com>
2018-11-15 15:27:49 +0900

    ~14장

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -1,9 +1,23 @@
+import java.util.Hashtable;
+
 /**
  * @author sangsik.kim
  */
 public class Bank {
 
+    private Hashtable rates = new Hashtable();
+
     public Money reduce(Expression source, String to) {
-        return source.reduce(to);
+        return source.reduce(this, to);
+    }
+
+    public void addRate(String from, String to, int rate) {
+        rates.put(new Pair(from, to), rate);
+    }
+
+    public int rate(String from, String to) {
+        if (from.equals(to)) return 1;
+        Integer rate = (Integer) rates.get(new Pair(from, to));
+        return rate;
     }
 }
--- src/main/java/Expression.java
+++ src/main/java/Expression.java
@@ -2,5 +2,5 @@
  * @author sangsik.kim
  */
 public interface Expression {
-    Money reduce(String to);
+    Money reduce(Bank bank, String to);
 }
